cfdb767817fa982beae5487a3981ec929c55206b
charitypepe <info@charitypepe.com>
2025-03-12 01:30:39 +0100

    Чисто качване без големи файлове

---
+++ www/main.js
@@ -0,0 +1,10 @@
+const startBtn = document.getElementById('start');
+const speakBtn = document.getElementById('speak');
+
+startBtn.onclick = () => {
+  alert('Бутонът "Запис" работи!');
+};
+
+speakBtn.onclick = () => {
+  alert('Бутонът "Говори" работи!');
+};
